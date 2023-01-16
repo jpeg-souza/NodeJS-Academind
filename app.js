a2942ec46ac39a506a684ec809a1f73c63291f2a
jpeg-souza <souza.jpeg@gmail.com>
2023-01-15 23:55:33 -0300

    Using Node modules system

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((request, response) =>{
-    response.setHeader('Content-Type', 'text/html')
-    if(request.url === '/'){
-        response.write('<html>');
-        response.write('<head><title>Enter Message</title><head>');
-        response.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        response.write('</html>');
-        return response.end();
-    }else if (request.url === '/message' && request.method === 'POST'){
-        const body = [];
-        request.on('data', (chunk) =>{
-            console.log(chunk)
-            body.push(chunk)
-        })
+const routes = require('./routes');
 
-        request.on('end', () =>{
-            const parsedBody = Buffer.concat(body).toString().split('=')[1]
-            console.log(parsedBody)
-            fs.writeFileSync('message.txt', parsedBody);
-            response.statusCode = 302
-            response.setHeader('Location', '/')
-            return response.end();
-        })
+// console.log(routes.someText)
 
-    }
-    response.end()
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
